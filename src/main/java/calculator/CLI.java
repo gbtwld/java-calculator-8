048a69c8fc660892348c6c43bdb382663e7106b3
gbtwld <gbtwld@gmail.com>
2025-10-20 19:49:54 +0900

    feat(CLI): 계산 결과 출력 기능 구현

--- src/main/java/calculator/CLI.java
+++ src/main/java/calculator/CLI.java
@@ -7,7 +7,15 @@ public class CLI {
     public CLI() {
     }
 
+    public void prompt() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
     public String read() {
         return Console.readLine();
+    }
+
+    public void printResult(long result) {
+        System.out.println("결과 : "  + result);
     }
 }
